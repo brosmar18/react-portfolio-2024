d0ee64383ec28c74144cf814f97fb7788b059e6c
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-17 17:51:11 -0600

    Header image styles updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import { hero } from '../assets/images';
 const Header = () => {
   return (
     <header className='flex flex-col gap-10 items-center justify-between lg:flex-row lg:gap-0'>
-      <img src={hero} alt="profile" className="h-[450px] z-30" />
+      <img src={hero} alt="profile" className="h-[400px] md:h-[600px] z-30" />
       <div className="w-1/2 flex flex-col gap-8 items-center justify-center">
-        <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold "> Developing Seamless
+        <h1 className="text-3xl lg:text-5xl font-bold "> Developing Seamless
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-            {' '} Solutions {' '}
-          </span>,
+            {' '} Solutions, {' '}
+          </span>
           From Front to Back.</h1>
         <p className="text-sm md:text-xl">Welcome to the intersection of functionality and creativity in my full-stack development journey. Here, you&apos;ll find a showcase of versatile projects, each a testament to my approach to building robust and aesthetically pleasing digital experiences.
         </p>
