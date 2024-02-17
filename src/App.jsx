8a72378ef41f9d2f63dab8bfe89c1715a0dea086
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-17 16:38:01 -0600

    Project structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { Home, Experience, Projects, Contact } from './pages';
+
+
 const App = () => {
  return (
-   <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+  <div className="bg-[#000B2A] text-[#fafafa] h-full">
+   <Router>
+    <Navbar />
+    <Routes>
+     <Route path='/' element={<Home />} />
+     <Route path='/experience' element={<Experience />} />
+     <Route path='/projects' element={<Projects />} />
+     <Route path='/contact' element={<Contact />} />
+    </Routes>
+    <Footer />
+   </Router>
+  </div>
  )
-}
+};
 
 export default App
