575e15ae60cd73dae788ccc07334617ffbea9c71
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-17 17:16:27 -0600

    Header added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
 import logo from './logo.png';
+import hero from './hero.png';
 
-export { logo };
+export { logo, hero };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Header from '../components/Header';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 mb-10 mt-10 h-[calc(100vh-12rem)]">
+      <Header />
+    </div>
   )
 }
 
