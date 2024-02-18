10dedbbf73ec8679bfdf093ee46c285d0ccf79a2
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-18 12:21:07 -0600

    Experience heading text animated

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,34 +1,60 @@
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+'use client';
+import { motion } from 'framer-motion';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { experiences } from '../constants';
-import { brewhemia } from '../assets/icons';
+
+
 
 const Experience = () => {
+
+  const textVariant = {
+    hidden: {
+      y: -20,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        duration: 1.25,
+        delay: 0.2,
+      }
+    }
+  };
+
+
   return (
     <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 mb-10 mt-10 h-full text-black">
-      <div>
-        <p className="sm:text-[18px] text-[14px] uppercase tracking-wider text-center">
+      <motion.div
+        initial='hidden'
+        animate='show'
+        variants={textVariant}
+      >
+        <p className="uppercase tracking-wider text-center text-white text-lg mb-1 md:text-xl">
           What I&#39;ve done
         </p>
-        <h2 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center mb-20">
+        <h2 className="text-white text-4xl lg:text-6xl font-bold text-center mb-0">
           Work Experience
         </h2>
-      </div>
-      <div className="mt-12">
+      </motion.div>
+      <div className="mt-20 md:mt-32">
         <VerticalTimeline>
           {experiences.map((experience) => (
             <VerticalTimelineElement
               key={`exp-timeline-${experience.company_name}`}
+              contentArrowStyle={{ borderRight: `7px solid ${experience.border}` }}
               date={experience.date}
               iconStyle={{ background: experience.iconBg }}
               icon={
                 <div className='flex justify-center items-center w-full h-full '>
-                <img
-                  src={experience.icon}
-                  alt={experience.company_name}
-                  className='w-[80%] h-[80%] object-contain'
-                />
-              </div>
+                  <img
+                    src={experience.icon}
+                    alt={experience.company_name}
+                    className='w-[80%] h-[80%] object-contain'
+                  />
+                </div>
               }
               contentStyle={{
                 borderBottom: '8px',
