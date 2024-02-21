5cd527ce0d9345852d2d72d6ae96e2a897758eef
brosmar18 <bryangonzalez1896@icloud.com>
2024-02-21 10:26:59 -0600

    coming son message added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 mb-10 mt-10 md:h-[calc(100vh-12rem)]">Contact</div>
+    <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 mb-10 mt-10 md:h-[calc(100vh-12rem)]">
+      <div className='w-full h-full flex flex-col gap-5 items-center justify-center'>
+        <h1 className='text-6xl font-bold'>COMING SOON</h1>
+        <p className='text-xl'>bryangonzalez1896@icloud.com</p>
+        <p className='text-xl'>(319) 777-6140</p>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 mb-10 mt-10 md:h-[calc(100vh-12rem)]">Projects</div>
+    <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 mb-10 mt-10 md:h-[calc(100vh-12rem)]">
+      <div className='w-full h-full flex items-center justify-center'>
+        <h1 className='text-6xl font-bold'>COMING SOON</h1>
+      </div>
+    </div>
   )
 }
 
